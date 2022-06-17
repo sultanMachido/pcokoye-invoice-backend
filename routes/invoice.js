7dc25ba3d30c0f8c054d7822e556086d8f16b350
Ope <opehardesina@gmail.com>
2022-06-17 17:48:32 +0100

    commit

--- routes/invoice.js
+++ routes/invoice.js
@@ -3,6 +3,13 @@ let router = express.Router();
 let Complaint = require('../models/Invoice');
 
 
+router.get('/invoice',(req,res)=>{
+    
+    res.json({
+        message:'OK'
+    })
+})
+
 
 router.post('/invoice',(req,res)=>{
     
